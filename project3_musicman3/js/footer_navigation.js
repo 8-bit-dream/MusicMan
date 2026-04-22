905ae9009829a00cba21a5ced791b51076840a67
8-bit-dream <Andrewaddens@gmail.com>
2026-04-21 22:14:58 -0400

    Delete project3_musicman3 directory

--- project3_musicman3/js/footer_navigation.js
+++
@@ -1,8 +0,0 @@
-//Purpose: Basic js file for navigation
-// Author: Andrew Addens
-
-
-
-document.write("<div style='text-align:center;'>");
-document.write("<a href='#intro'>Homepage</a> | <a href='#work'>Notes</a> | <a href='#work2'>Scales</a> | <a href='#work3'>Rhythm</a> | <a href='#contact'>Contact</a>");
-document.write("</div>");
